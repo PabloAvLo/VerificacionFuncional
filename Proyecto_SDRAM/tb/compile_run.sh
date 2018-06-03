002ee9fd8b391be04983af036f8be1bf3727c7d3
Heberth Valverde <hgvaldela@gmail.com>
2018-06-03 13:03:14 -0600

    Included all needed files to compile

--- Proyecto_SDRAM/tb/compile_run.sh
+++ Proyecto_SDRAM/tb/compile_run.sh
@@ -6,7 +6,7 @@ g++ -fpic -c sc_tb_vpi.c sc_vpi.c sc_tb.cpp -I /usr/include/iverilog/ -I /usr/lo
 echo 'Creating vpi file'
 g++ -shared -L /usr/local/systemc-2.3.2/lib-linux64  -Wl,-rpath=/usr/local/systemc-2.3.2/lib-linux64 -o sc_tb_vpi.vpi *.o -lsystemc -lm -lvpi
 echo 'Compiling verilog files'
-iverilog -o tb.vvp tb.v ../rtl/top/sdrc_top.v
+iverilog -o tb.vvp tb.v ../rtl/top/sdrc_top.v ../rtl/wb2sdrc/wb2sdrc.v ../rtl/core/sdrc_core.v ../rtl/core/sdrc_bank_ctl.v ../rtl/core/sdrc_bank_fsm.v ../rtl/core/sdrc_req_gen.v ../rtl/core/sdrc_xfr_ctl.v ../rtl/core/sdrc_bs_convert.v ../rtl/lib/async_fifo.v
 echo 'Simulation Started'
 vvp -n -M. -msc_tb_vpi tb.vvp
 echo 'Simulation Ended'

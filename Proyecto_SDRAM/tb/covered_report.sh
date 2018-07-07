ed093f2ee2caca1f1d0b0fb948b9ddd6ef93a053
Heberth Valverde <hgvaldela@gmail.com>
2018-07-07 16:37:55 -0600

    Flags to run 32 bit mode

--- Proyecto_SDRAM/tb/covered_report.sh
+++ Proyecto_SDRAM/tb/covered_report.sh
@@ -1,4 +1,4 @@
 echo 'Running coverage analysis'
-covered score -I . -t tb -v tb.v -v ../rtl/top/sdrc_top.v -v ../rtl/wb2sdrc/wb2sdrc.v -v ../rtl/core/sdrc_bank_ctl.v -v ../rtl/core/sdrc_bank_fsm.v -v ../rtl/core/sdrc_req_gen.v -v ../rtl/core/sdrc_xfr_ctl.v -v ../rtl/core/sdrc_bs_convert.v -v ../rtl/lib/async_fifo.v -v ../rtl/core/sdrc_core.v -e mt48lc8m8a2 -vcd verilog.vcd -o cov5.cdd
+covered score -I . -t tb -v tb.v -v ../rtl/top/sdrc_top.v -v ../rtl/wb2sdrc/wb2sdrc.v -v ../rtl/core/sdrc_bank_ctl.v -v ../rtl/core/sdrc_bank_fsm.v -v ../rtl/core/sdrc_req_gen.v -v ../rtl/core/sdrc_xfr_ctl.v -v ../rtl/core/sdrc_bs_convert.v -v ../rtl/lib/async_fifo.v -v ../rtl/core/sdrc_core.v -e mt48lc8m8a2 -e mt48lc2m32b2 -e IS42VM16400K -vcd verilog.vcd -o cov32.cdd
 echo 'Generating coverage report'
-covered report cov5.cdd
+covered report cov32.cdd
